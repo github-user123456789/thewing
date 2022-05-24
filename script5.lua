f6c3084f5fd5f28d0f70893fa4ca39fbd20ea3aa
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-05-24 20:06:15 +0100

    Update script5.lua

--- script5.lua
+++ script5.lua
@@ -2,11 +2,6 @@
 local function runner(cr, V3, BC, model, ylevel)
 -- MORE WALLS
 cr({"Part", model}, {
-	Size = V3(1, 7, 1),
-	Position = V3(-76.49996185302734, 7.700000762939453, -109.00248718261719),
-	Material = "Plastic",
-	BrickColor = BC("Black"),
-}); cr({"Part", model}, {
 	Size = V3(1, 1, 15),
 	Position = V3(-76.49996185302734, 11.699997901916504, -102.00249481201172),
 	Material = "Plastic",
@@ -43,18 +38,56 @@ cr({"Part", model}, {
 	Position = V3(-76.49996185302734, 3.700000762939453, -102.00249481201172),
 	Material = "Plastic",
 	BrickColor = BC("Black"),
-});
--- MORE METAL WALLS
-cr({"Part", model}, {
-	Size = V3(8, 14, 1),
-	Position = V3(-121.99993896484375, 7.700000762939453, -87.50286865234375),
-	Material = "DiamondPlate",
-	BrickColor = BC("Dark stone grey"),
 }); cr({"Part", model}, {
-	Size = V3(1, 14, 17),
-	Position = V3(-125.49993896484375, 7.700040817260742, -78.50411987304688),
-	Material = "DiamondPlate",
-	BrickColor = BC("Dark stone grey"),
+	Size = V3(5, 42.19999694824219, 1),
+	Position = V3(-101.50009155273438, 21.800003051757812, -185.4981231689453),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(1, 42.20000076293945, 6),
+	Position = V3(-104.50016784667969, 21.80000114440918, -187.99954223632812),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(8, 42.20000076293945, 1),
+	Position = V3(-79.0001449584961, 21.799924850463867, -185.49740600585938),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(1, 42.20000076293945, 30),
+	Position = V3(-74.50007629394531, 21.80000114440918, -199.998291015625),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(30, 42.20000076293945, 1),
+	Position = V3(-89.00006866455078, 21.80000114440918, -215.49803161621094),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(1, 42.20000076293945, 9),
+	Position = V3(-104.50008392333984, 21.80000114440918, -211.49917602539062),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(1, 13.800000190734863, 16),
+	Position = V3(-104.5001220703125, 36.000003814697266, -198.99765014648438),
+	Material = "SmoothPlastic",
+	BrickColor = BC("Ghost grey"),
+}); cr({"Part", model}, {
+	Size = V3(1, 4, 11),
+	Position = V3(-74.49995422363281, 12.699997901916504, -141.50123596191406),
+	Material = "Slate",
+	BrickColor = BC("Navy blue"),
+}); cr({"Part", model}, {
+	Size = V3(1, 4, 11),
+	Position = V3(-74.49996948242188, 2.700000762939453, -141.50157165527344),
+	Material = "Slate",
+	BrickColor = BC("Navy blue"),
+}); cr({"Part", model}, {
+	Size = V3(1, 7, 1),
+	Position = V3(-76.49996185302734, 7.700000762939453, -109.00248718261719),
+	Material = "Plastic",
+	BrickColor = BC("Black"),
 });
 end
 
